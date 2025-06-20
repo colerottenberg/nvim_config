7b80fb466912ca55f8507afdd3be9d54caa52971
Cole Rottenberg <Cole.Rottenberg@Honeywell.com>
2025-06-20 11:11:11 -0400

    ignorigin allowed

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -22,11 +22,13 @@ return {
       -- control auto formatting on save
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          "go",
-          "python",
-          "lua",
-        },
+        -- allow_filetypes = { -- enable format on save for specified filetypes only
+        --   "go",
+        --   "python",
+        --   "lua",
+        --   "json",
+        --   "rust",
+        -- },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
         },
