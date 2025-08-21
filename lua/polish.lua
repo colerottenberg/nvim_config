7a0a01585aacd31e3303c5e1fa01f2b505d8e500
Cole Rottenberg <Cole.Rottenberg@Honeywell.com>
2025-08-21 15:13:59 -0400

    detect windows and running powershell instead of cmd

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,15 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+local o = vim.o
+if jit.os == "Windows" then
+  o.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
+  o.shellcmdflag =
+    "-NoLogo -NoProfile -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  o.shellquote = ""
+  o.shellxquote = ""
+end
