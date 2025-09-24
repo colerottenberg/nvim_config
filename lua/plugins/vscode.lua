24c6507bd32480db41cb12a097ecbdfb1294f994
Cole Rottenberg <Cole.Rottenberg@Honeywell.com>
2025-09-24 10:02:55 -0400

    fixing vscode integration

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -1,29 +1,7 @@
 -- don't do anything in non-vscode instances
 if not vim.g.vscode then return {} end
 
--- a list of known working plugins with vscode-neovim, update with your own plugins
-local plugins = {
-  "lazy.nvim",
-  "AstroNvim",
-  "astrocore",
-  "astroui",
-  "Comment.nvim",
-  "nvim-autopairs",
-  "nvim-treesitter",
-  "nvim-ts-autotag",
-  "nvim-treesitter-textobjects",
-  "nvim-ts-context-commentstring",
-}
-
-local Config = require "lazy.core.config"
--- disable plugin update checking
-Config.options.checker.enabled = false
-Config.options.change_detection.enabled = false
--- replace the default `cond`
-Config.options.defaults.cond = function(plugin) return vim.tbl_contains(plugins, plugin.name) end
-
 -- fixing vscode-neovim message output
-cmdheight = 3
 
 ---@type LazySpec
 return {
@@ -34,9 +12,8 @@ return {
     opts = {
       mappings = {
         n = {
-          ["<Leader>ff"] = "<CMD>Find<CR>",
-          ["<Leader>fw"] = "<CMD>call VSCodeNotify('workbench.action.findInFiles')<CR>",
-          ["<Leader>ls"] = "<CMD>call VSCodeNotify('workbench.action.gotoSymbol')<CR>",
+          L = "<CMD>Tabnext<CR>",
+          H = "<CMD>Tabprevious<CR>",
         },
       },
     },
