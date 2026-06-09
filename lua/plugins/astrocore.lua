59a6996b8b5089a4011a7fee8bd8120a157673e7
Cole Rottenberg <cole.rottenberg@honeywell.com>
2026-06-09 15:15:14 -0400

    Adding lazyjj as toggleterm keybinding

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -5,6 +5,15 @@
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 -- as this provides autocomplete and documentation while editing
 
+local astro = require "astrocore"
+local lazyjj = {
+  callback = function()
+    local worktree = astro.file_worktree()
+    astro.toggle_term_cmd { cmd = "lazyjj ", direction = "float" }
+  end,
+  desc = "ToggleTerm lazyjj",
+}
+
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
@@ -12,12 +21,12 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
+      large_buf = { size = 1024 * 256, lines = 10000 },             -- set global limits for large files for disabling features like treesitter
+      autopairs = true,                                             -- enable autopairs at start
+      cmp = true,                                                   -- enable completion at start
       diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      highlighturl = true,                                          -- highlight URLs at start
+      notifications = true,                                         -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -40,15 +49,15 @@ return {
     -- },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {                   -- vim.opt.<key>
         relativenumber = false, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = true, -- sets vim.opt.wrap
-        exrc = true, -- enabling excr to read .nvim.lua
+        number = true,          -- sets vim.opt.number
+        spell = false,          -- sets vim.opt.spell
+        signcolumn = "yes",     -- sets vim.opt.signcolumn to yes
+        wrap = true,            -- sets vim.opt.wrap
+        exrc = true,            -- enabling excr to read .nvim.lua
       },
-      g = { -- vim.g.<key>
+      g = {                     -- vim.g.<key>
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
@@ -275,6 +284,10 @@ return {
           function() require("astrocore").reload() end,
           desc = "Reload neovim",
         },
+
+        -- Adding LazyJJ Tui for Jujustu
+        ["<Leader>jj"] = lazyjj,
+        ["<Leader>tj"] = lazyjj,
         -- mappings seen under group name "Buffer"
         -- ["<Leader>bd"] = {
         --   function()
