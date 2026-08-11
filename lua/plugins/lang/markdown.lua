07926479311f9f0270b0ce19863721ce257727e6
Cole Rottenberg <cole.rottenberg@honeywellaerospace.us>
2026-08-11 16:27:19 -0400

    Adding markdown configuration and moving powershell plugin to verylazy

---
+++ lua/plugins/lang/markdown.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- use latest release, remove to use latest commit
+    ft = 'markdown',
+    cmd = 'Obsidian',
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      picker = {
+        name = 'snacks.picker',
+      },
+      legacy_commands = false, -- this will be removed in 4.0.0
+      workspaces = {
+        {
+          name = 'resnav',
+          path = '~/Documents/ResNav',
+        },
+        {
+          name = 'personal',
+          path = '~/Documents/Personal',
+        },
+      },
+    },
+    dependencies = {
+      'folke/snacks.nvim',
+    },
+  },
+  -- automatic bulleted lists
+  ---@type LazySpec
+  {
+    'bullets-vim/bullets.nvim',
+    ft = { 'markdown' },
+    ---@type bullets.Config
+    opts = {
+      enabled_file_types = { 'markdown', 'text', 'gitcommit' },
+    },
+  },
+  --- Rendering Markdown
+  ---@type LazySpec
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = 'markdown',
+    cmd = 'RenderMarkdown',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    keys = {
+      { '<Leader>um', '<Cmd>RenderMarkdown toggle<CR>', desc = 'Toggle markdown rendering' },
+    },
+    opts = {
+      file_types = { 'markdown' },
+      completions = { lsp = { enabled = true } },
+    },
+  },
+}
--- lua/plugins/powershell.lua
+++ lua/plugins/powershell.lua
@@ -1,5 +1,6 @@
 return {
   'TheLeoP/powershell.nvim',
+  ft = 'ps1',
   ---@type powershell.user_config
   opts = {
     bundle_path = vim.fn.stdpath('data'),
