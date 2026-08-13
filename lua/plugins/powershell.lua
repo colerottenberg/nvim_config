1b43c0f6f96d3835081cc3d267af0be0f40bf86d
Cole Rottenberg <cole.rottenberg@honeywellaerospace.us>
2026-08-13 08:44:06 -0400

    disabling powershell plugin unless available

--- lua/plugins/powershell.lua
+++ lua/plugins/powershell.lua
@@ -1,9 +1,11 @@
 return {
   'TheLeoP/powershell.nvim',
-  ft = 'ps1',
+  cond = function()
+    return vim.fn.has('win32') == 1
+  end,
+  -- ft = 'ps1',
   ---@type powershell.user_config
   opts = {
     bundle_path = vim.fn.stdpath('data'),
   },
-  enabled = vim.fn.has('win32'),
 }
