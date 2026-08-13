89094da00e5d5c0d40ef0b5ca7305e3289038ae2
Cole Rottenberg <cole.rottenberg@gmail.com>
2026-08-12 23:22:56 -0400

    adding onedark and competitest

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,64 +1,77 @@
 return {
-  'kawre/leetcode.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'MunifTanjim/nui.nvim',
-    'folke/snacks.nvim',
+  {
+    'kawre/leetcode.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'folke/snacks.nvim',
+    },
+    ---@type lc.UserConfig
+    opts = {
+      lang = 'c',
+    },
+    event = 'VeryLazy',
+    keys = {
+      { '<Leader>Lm', vim.cmd.Leet, desc = 'Leetcode Menu' },
+      {
+        '<Leader>Lc',
+        function()
+          local lc = require('leetcode.command')
+          lc.change_lang()
+        end,
+        desc = 'change language',
+      },
+      {
+        '<Leader>Ls',
+        function()
+          local lc = require('leetcode.command')
+          lc.q_submit()
+        end,
+        desc = 'submit',
+      },
+      {
+        '<Leader>Lr',
+        function()
+          local lc = require('leetcode.command')
+          lc.q_run()
+        end,
+        desc = 'run test cases',
+      },
+      {
+        '<Leader>Lh',
+        function()
+          local lc = require('leetcode.command')
+          lc.hints()
+        end,
+        desc = 'hints',
+      },
+      {
+        '<Leader>Lp',
+        function()
+          local lc = require('leetcode.command')
+          lc.desc_toggle()
+        end,
+        desc = 'toggle description',
+      },
+      {
+        '<Leader>Le',
+        function()
+          local lc = require('leetcode.command')
+          lc.exit()
+        end,
+        desc = 'exit',
+      },
+    },
   },
-  ---@type lc.UserConfig
-  opts = {
-    lang = 'c',
-  },
-  event = 'VeryLazy',
-  keys = {
-    { '<Leader>Lm', vim.cmd.Leet, desc = 'Leetcode Menu' },
-    {
-      '<Leader>Lc',
-      function()
-        local lc = require('leetcode.command')
-        lc.change_lang()
-      end,
-      desc = 'change language',
-    },
-    {
-      '<Leader>Ls',
-      function()
-        local lc = require('leetcode.command')
-        lc.q_submit()
-      end,
-      desc = 'submit',
-    },
-    {
-      '<Leader>Lr',
-      function()
-        local lc = require('leetcode.command')
-        lc.q_run()
-      end,
-      desc = 'run test cases',
-    },
-    {
-      '<Leader>Lh',
-      function()
-        local lc = require('leetcode.command')
-        lc.hints()
-      end,
-      desc = 'hints',
-    },
-    {
-      '<Leader>Lp',
-      function()
-        local lc = require('leetcode.command')
-        lc.desc_toggle()
-      end,
-      desc = 'toggle description',
-    },
-    {
-      '<Leader>Le',
-      function()
-        local lc = require('leetcode.command')
-        lc.exit()
-      end,
-      desc = 'exit',
-    },
+
+  {
+    'xeluxee/competitest.nvim',
+    dependencies = 'MunifTanjim/nui.nvim',
+    event = 'VeryLazy',
+    config = function()
+      ---@type competitest.Config
+      local opts = {}
+      require('competitest').setup()
+    end,
   },
 }
