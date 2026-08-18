70ee89f57ee7848794230370257ee1a14314c4b4
Cole Rottenberg <cole.rottenberg@honeywell.com>
2026-08-18 18:16:53 -0400

    adding more configuration to cmake-tools and removing after nvim

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -273,7 +273,7 @@ return {
 
     -- Inline variable values (virtual text) during a debug session.
     require('nvim-dap-virtual-text').setup({
-      enabled = false,
+      enabled = true,
       enabled_commands = true,
       highlight_changed_variables = true,
       show_stop_reason = true,
